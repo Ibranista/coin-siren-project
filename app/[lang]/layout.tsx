aad9cbe8e49fdcd5743267867c4f057dbf78b6d5
Ibrahim Kedir <techofreact@gmail.com>
2024-10-18 00:44:19 +0300

    ft: add some data to json.

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
   lang: string;
 }>) {
-  console.log('hello')
+
   return (
     <html lang={lang} className={poppins.className}>
       <body>
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import Text from "./components/Atoms/text";
 import Middlegrid from "./components/Molecules/middlegrid";
 import Middlegrid2 from "./components/Molecules/middlegrid2";
 
 export default async function Home({ params }: { params: { lang: string } }) {
   const locale = params.lang || 'en-US';
   const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
-  const url = `${baseUrl}/api/sample-data-en?locale=${locale}`;
+  const url = `${baseUrl}/api/nav-data?locale=${locale}`;
 
   const response = await fetch(url);
   const data = await response.json();
